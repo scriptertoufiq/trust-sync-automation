7085f74c4d427eb963df511deb3a4ff047031f8a
scriptertoufiq <scriptertoufiq@gmail.com>
2025-01-07 16:20:08 +0600

    fixed the gitignore issue

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 test.use({ storageState: 'playwright/.auth/user.json' });
 
 
-test('Dashboard Checking Of Shopify', async ({ page }) => {
+test('Dashboard Checking Of Shopify ', async ({ page }) => {
   const cookies = await page.context().cookies();
   console.log(cookies); // Check if Shopify-related cookies are present
 
